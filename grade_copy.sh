00dda9f375e5e93bf56269e49240630d239ecbb1
pcedro <pcedro@ucsd.edu>
2024-03-12 12:16:25 -0700

    updated bash script with syna

--- grade_copy.sh
+++ grade_copy.sh
@@ -40,7 +40,8 @@ successes=$(grep -e "OK" junit-output.txt)
 if ! (( -z $successes ))
 then
     IMPORTS=$(grep -n "import" student-submission/ListExamples.java)
-    if ! [ -n "$IMPORTS" ] && [[ ! "$IMPORTS" =~ (ArrayList|List) ]]
+    if [ "$IMPORTS" != "import java.util.ArrayList;"] || [ "$IMPORTS" != "import java.util.List;
+"]
     then
         echo "Illegal imports"
         echo "Score: 0%"
@@ -48,18 +49,13 @@ then
     fi
     echo "Score: 100%"
     exit
-fi
 failed=$(grep -e "Tests" -e "Failure" junit-output.txt)
 cat junit-output.txt
 IMPORTS = grep -n "import" student-submission/ListExamples.java
-# if (imports!="" and (imports does not contain arraylist or list))
-if ! [ -n "$IMPORTS" ] && [[ ! "$IMPORTS" =~ (ArrayList|List) ]]
+if [ "$IMPORTS" != "import java.util.ArrayList;"] || [ "$IMPORTS" != "import java.util.List;
+"]
 then
     echo "Illegal imports"
     echo "Score: 0%"
     exit
 fi
-
-TESTSRUN= $(grep -oE "Tests run: [0-9]+" junit-output.txt)
-TESTSFAILED= $(grep -oE "Failures: [0-9]+" junit-output.txt)
-echo "Score: " + 100*$($TESTSRUN-$TESTSFAILED)/$TESTSRUN
